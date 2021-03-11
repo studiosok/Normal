24d93edc58c19c88d8695956afed19ee21da78be
kristen <studiosok@gmail.com>
2021-03-11 12:56:05 -0500

    Basic structuring of home page

---
+++ src/Components/Age.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import icon from '../assets/icon.svg';
+
+const Age = () => {
+  return (
+    <div>
+      <h3>"Normal" Age</h3>
+    </div>
+  )
+
+}
+
+export default Age
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import icon from '../assets/icon.svg';
+import Age from './Components/Age'
+import Behavior from './Components/Behavior'
 
 const Home = () => {
   return (
     <div>
-      <h1>Rendering Normal</h1>
+      <div className="Main">
+       <h1>Normal</h1>
+      </div>
+      <div className="Main">
+        <button>Common Behavior Questions</button>
+      </div>
     </div>
   )
 
